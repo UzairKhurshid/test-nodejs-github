22f641631b0ef9b5a6b9570e42b68627ce9b1306
uzair khurshid <sardar.uzair12@gmail.com>
2020-11-25 17:16:25 +0500

    ad

--- server.js
+++ server.js
@@ -7,16 +7,21 @@ const app=express()
 const PORT=process.env.PORT || 8080
 
 app.get('/',(req,res)=>{
-    console.log('testing gitlab')
-    res.send('testing gitlab')
+    console.log('testing github')
+    res.send('testing github')
 })
 app.get('/2',(req,res)=>{
-    console.log('testing gitlab2')
-    res.send('testing gitlab2')
+    console.log('testing github2')
+    res.send('testing github2')
 })
 app.get('/3',(req,res)=>{
-    console.log('testing gitlab3')
-    res.send('testing gitlab3')
+    console.log('testing github3')
+    res.send('testing github3')
+})
+
+app.get('/4',(req,res)=>{
+    console.log('testing github4  app')
+    res.send('testing github4  app')
 })
 
 
